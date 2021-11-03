9ac4a6cb66954789e830ed99a6e30cbd851fec91
Ezhko <ucsd.camp.21@gmail.com>
2021-11-03 14:02:09 -0700

    add frontend js to handle answer

--- controllers/api/magic-router.js
+++ controllers/api/magic-router.js
@@ -1,41 +1,24 @@
 const router = require("express").Router();
 const withAuth = require("../../util/withAuth");
 const { History } = require("../../models");
-// const { User } = require("../../models");
 const helper = require("../../util/helpers");
 
-// client post question: POST req from form AND from LINK on 8ball
 
-router.put("/magic", withAuth, async (req, res) => {
-  const question = req.params;
-  console.log(question);
+router.post("/", withAuth, async (req, res) => {
+  
+  const question = req.body.question;
+  
   try {
-    // save question in history db
-
-
-   const question_db = await History.update({ question: question }, {
-    
-    where: {
-      user_id: req.session.userId,
-    },
-  });
-console.log(question_db);
-    // get answer from helpers.js
-
+    const user_id = req.session.userId;
     const answer = helper;
+  
+   const history = await History.create({
+      question: question,
+      answer: answer,
+      user_id: user_id,
+    });
 
-    // save answer to history db
-
-const answer_db =  await History.update({ answer: answer }, {
-    
-    where: {
-      user_id: req.session.userId,
-    }}); 
-console.log(answer_db);
-    // send answer to magic.handlebars
-
-    res.send(answer);
-
+    res.json(history);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error." });
--- util/helpers.js
+++ util/helpers.js
@@ -1,9 +1,10 @@
-module.exports = {
-  // add helper function for picking random answer for handlebars here
-  answer: "NOT THE ANSWER"
-// const array of answers
-// pick random answer from array
-// return random answer
+function generator () {
+  const answers=['Yes!','No!','Go Do It!','Very Doubtful!', 'No Way!'];
+  const random = Math.floor(Math.random() * answers.length);
+  const answer = answers[random];
+  return answer;
+}
 
-
-};
+// const answer = generator();
+// console.log(answer);
+module.exports = generator();
