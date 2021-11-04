e609a61958a50dcf0c743c6c135bc3709b6deba1
Yi Liu <elenaliu0415@gmail.com>
2021-11-04 11:40:00 -0700

    fix format time

--- util/helpers.js
+++ util/helpers.js
@@ -2,7 +2,7 @@ module.exports = {
     // The custom helper 'format_date' takes in a timestamp
     format_date: (date) => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-          new Date(date).getFullYear()} ${new Date(date).getTime()}
+          new Date(date).getFullYear()} ${new Date(date).toLocaleTimeString()}
         `;
       }
   };
